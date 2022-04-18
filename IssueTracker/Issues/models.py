04cdb56a4446c57c91c534f1f22b944f6bddc00d
BenJensen2 <btjensen@mtu.edu>
2022-04-18 16:45:38 -0500

    Create Home page route and Models (Issue, Sprint, Project)

--- IssueTracker/Issues/models.py
+++ IssueTracker/Issues/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+# Single issue
+class Issue(models.Model):
+    # Identifying Information
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    # Projections
+    projected_start = models.DateTimeField()
+    projected_end = models.DateTimeField()
+    
+    # Actuals
+    actual_start = models.DateTimeField()
+    actual_end = models.DateTimeField()
+
+    # Relational Information
+    sprint = models.ForeignKey('Sprint', on_delete=models.CASCADE)
+
+    # Creation & Update
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Sprint(models.Model):
+  # Identifying Information
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    # Projections
+    projected_start = models.DateTimeField()
+    projected_end = models.DateTimeField()
+    
+    # Actuals
+    actual_start = models.DateTimeField()
+    actual_end = models.DateTimeField()
+
+    # Relational Information
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    # Issues
+
+    # Creation & Update
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Project(models.Model):
+    # Identifying Information
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    # Projections
+    projected_start = models.DateTimeField()
+    projected_end = models.DateTimeField()
+    
+    # Actuals
+    actual_start = models.DateTimeField()
+    actual_end = models.DateTimeField()
+
+    # Relational Information
+    # Sprints
+
+    # Creation & Update
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
--- IssueTracker/Issues/views.py
+++ IssueTracker/Issues/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
   return HttpResponse('Captain, we have liftoff')
+
+def home(request):
+  return render(request, 'home.html')
