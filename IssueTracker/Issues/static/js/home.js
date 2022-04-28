302329ec8266886606652baf8bb396928b6dd9a2
BenJensen2 <btjensen@mtu.edu>
2022-04-28 15:37:29 -0500

    Click outside of New Project Form hides pop-ups

--- IssueTracker/Issues/static/js/home.js
+++ IssueTracker/Issues/static/js/home.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
   console.log("Project Form");
 
+  // ####################################################################
+  // Click Events
+  // ####################################################################
   // New Project Form Popup
   $("#new_project").click(function() {
     $(".pop-ups").css("display", "flex");
   });
 
   // New Project Form Hide
+
   // - Cancel Button
   $(".cancel-button, .add-project").click(function() {
     $(".pop-ups").css("display", "none");
   });
+
+  // - Click outside of form
+  document.addEventListener("click", function(e) {
+    let popUpWrapper = document.getElementsByClassName("pop-ups")[0];
+    if (popUpWrapper == e.target) {
+      $(".pop-ups").css("display", "none");
+    }
+  });
+
+
+
+
+  
 });
