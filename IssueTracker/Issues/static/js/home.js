39d6ba656b83c144253e47aa0b29b1367d3a897a
BenJensen2 <btjensen@mtu.edu>
2022-04-29 11:33:56 -0500

    Project Form Updates

--- IssueTracker/Issues/static/js/home.js
+++ IssueTracker/Issues/static/js/home.js
@@ -4,51 +4,67 @@ $(document).ready(function() {
   // ####################################################################
   // Click Events
   // ####################################################################
-  
+
   // Open Pop-up Form Wrappers
-  function openPopUp(){
-    console.log("Pop-ups opened")
+  function openPopUp() {
+    console.log("Pop-ups opened");
     $(".pop-ups").css("display", "flex");
     $(".pop-up-form").css("display", "flex");
   }
 
   // Close Pop-up Form Wrappers
-  function closePopUp(){
+  function closePopUp() {
+    // Hide Wrappers
     $(".pop-ups").css("display", "none");
     $(".pop-up-form").css("display", "none");
+    // Set form action & method to get homepage
+    $(".pop-up-form").children().attr("action", "/");
+    $(".pop-up-form").children().attr("method", "get");
   }
-  
-  // New Project Form Popup
-  $("#new_project").click(function() {
-    openPopUp()
-    $(".new_project_form").css("display", "inline-block");
-  });
 
-    // Project Detail Form Popup
-    $(".project-title").click(function(){
-      openPopUp()
+  // New Project Form Popup
+  function newProjectClick() {
+    $("#new_project").click(function() {
+      // Opens the Pop-Up Wrappers
+      openPopUp();
+      // Opens the project detail form
       $(".project-detail-form").css("display", "inline-block");
-    })
+      // Changes the form action to "create_project" and method to "post"
+      $(".pop-up-form").children().attr("action", "create_project");
+      $(".pop-up-form").children().attr("method", "post");
+    });
+  }
+  newProjectClick()
+
+  // Project Detail Form Popup
+  $(".project-title").click(function() {
+    openPopUp();
+    $(".project-detail-form").css("display", "inline-block");
+  });
 
   // New Project Form Hide
   // - Cancel Button
   $(".cancel-button, .add-project").click(function() {
-    closePopUp()
+    closePopUp();
     $(".new_project_form").css("display", "none");
   });
   // - Click outside of form
   document.addEventListener("click", function(e) {
-    e.preventDefault()
+    e.preventDefault();
     let popUpWrapper = document.getElementsByClassName("pop-ups")[0];
-    if (popUpWrapper == e.target) {
-      closePopUp()
+    let newProjectButton = document.getElementById("new_project")
+    console.log(e.target)
+    if (e.target == newProjectButton){
+      newProjectClick()
+    }
+    else if (popUpWrapper == e.target) {
+      closePopUp();
       $(".new_project_form").css("display", "none");
     }
   });
 
   // Delete Project
   $(".project-title .delete-button").click(function() {
-    console.log("Delete Project")
-  })
-
+    console.log("Delete Project");
+  });
 });
